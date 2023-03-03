d18694986eee0f29cc43e75988e6961267feb907
Afroza khanam jeny <afrozakhanomjeny@gmail.com>
2023-03-04 01:31:16 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+function test(){
+	document.getElementById('p').innerHTML = Date() ;
+	
+}
+
+function on(){
+	document.getElementById('img').src = 'images/on.gif';
+}
+
+function off(){
+	document.getElementById('img').src = 'images/off.gif';
+}
+
+function none(){
+	document.getElementById('n').style.display = 'none';
+}
+
+
+function none(){
+var  x = 10;
+// Here x is 10
+
+{  
+var x = 2;
+// Here x is 2
+}
+
+// Here x is 2
+document.getElementById("demo").innerHTML = x;
+}
+
+function ev(){
+
+document.getElementById('ev').innerHTML = Date() ;
+}
+function e(){
+	var number1 = 5;
+	var number2 = 10;
+
+	number1+= number2
+
+}
+function demo(){
+document.getElementById("demo").innerHTML =
+answer1 + "<br>" + answer2 + "<br>" + answer3; 
+}
+
+function a(){
+	let x = 10;
+document.getElementById("a").innerHTML = "Value of x is: " + x;
+}
+
+function v(){
+	 let x; x = 6;
+document.getElementById("v").innerHTML = 'x';
+}
+function op(){
+	
+document.getElementById("op").innerHTML = (5 + 6) * 10;
+}
+
+function font(){
+	document.getElementById('p').style.fontSize = '50px';
+}
